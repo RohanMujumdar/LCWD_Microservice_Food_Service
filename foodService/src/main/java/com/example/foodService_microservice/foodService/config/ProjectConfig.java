e2e8bbc0d7a822421fcf622639d293f83bf82df5
Rohan Mujumdar <rohanmujumdar2001@gmail.com>
2025-06-22 16:53:49 +0530

    Food Category's table name changed.

--- foodService/src/main/java/com/example/foodService_microservice/foodService/config/ProjectConfig.java
+++ foodService/src/main/java/com/example/foodService_microservice/foodService/config/ProjectConfig.java
@@ -1,8 +1,7 @@
 package com.example.foodService_microservice.foodService.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
